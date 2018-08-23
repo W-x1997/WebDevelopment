3e88c5c6c42a18f62869f570ef49825131e9c0fd
wx <421980589@qq.com>
2018-08-23 17:53:46 +0800

    logic

--- homework9.js
+++ homework9.js
@@ -1,3 +1,16 @@
-var age=prompt("Please enter your age:");
-var time=age*365*24;
-alert("You have been alive for"+time+"hours!!!");
+// var age=prompt("Please enter your age:");
+// var time=age*365*24;
+// alert("You have been alive for"+time+"hours!!!");
+
+
+var myage=21;
+var show="Please guess my age!!";
+alert(show);
+var guess=Number(prompt("Please Guess:"));
+if(guess<myage){
+  console.log("Too low!");
+}else if(guess>myage){
+	console.log("Too high!");
+}else{
+		console.log("You got it!");
+}
