4c91d8a211014451de7dd4886330bc1160326340
wx <421980589@qq.com>
2018-08-25 22:09:31 +0800

    foreach

--- solution.js
+++ solution.js
@@ -38,7 +38,52 @@ function kebabToSnake(str) {
 }
 
 
+function printReverse(num){
+   for(var k=num.length-1;i>=0;i--){
+     console.log(num[k]);
+   }
+
+ }
 
 
+
+// function isUniform(arr){
+//    for(var i=0;i<arr.length-1;i++){
+//     if(arr[i]!=arr[i+1])
+//       return false;
+//    }
+//    return true;
+
+// }
+
+
+function isUniform(arr){
+   var first=arr[0];
+   arr.forEach(function(element){
+     if(element!=first)
+      return false;
+   });
+    
+
+}
+
+
+
+function sumArray(arr){
+  var sum=0;
+  arr.forEach(function(element){
+    sum=sum+element;
+  }); 
+  return sum;
+}
+
+function max(arr){
+ var max=0;
+ arr.forEach(function(element){
+  if(max<element)
+    max=element;
+ });
+
+}
 
 
