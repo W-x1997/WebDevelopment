872a94354952c3fa664b0a562210c4f4499201a4
wx <421980589@qq.com>
2018-09-14 23:06:32 +0800

    -2

--- Todo List App/assets/js/todos.js
+++ Todo List App/assets/js/todos.js
@@ -1,16 +1,26 @@
 //Check off Specfic Todos By clicking
-$("li").click(function(){
-	if($(this).css("color")==="rgb(128,128,128)"){
-		$(this).css({
-			color:"black",
-			textDecoration:"none"
-		});
-	}else{
-           $(this).css({
-			color:"gray",
-			textDecoration:"line-through"
-		});
+// $("li").click(function(){
+// 	if($(this).css("color")==="rgb(128,128,128)"){
+// 		$(this).css({
+// 			color:"black",
+// 			textDecoration:"none"
+// 		});
+// 	}else{
+//            $(this).css({
+// 			color:"gray",
+// 			textDecoration:"line-through"
+// 		});
 
-	}
+// 	}
 	
+// });
+$("li").click(function(){
+$(this).toggleClass("completed");	
+	
+});
+
+
+//click on X to delete Todo
+$("span").click(function(){
+	alert("hhh");
 });
