e90c13e2f60d99b46684924dc0cc6a19aebbf37e
wx <421980589@qq.com>
2018-09-17 10:59:27 +0800

    logic finished and replace the 'click()' with 'on()'

--- Todo List App/assets/js/todos.js
+++ Todo List App/assets/js/todos.js
@@ -14,13 +14,43 @@
 // 	}
 	
 // });
-$("li").click(function(){
+$("ul").on("click","li",function(){
 $(this).toggleClass("completed");	
 	
 });
 
 
 //click on X to delete Todo
-$("span").click(function(){
-	alert("hhh");
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	
+		event.stopPropagation() 
+	}	);
+
+
+$("input[type='text']").keypress(function(event){
+	if(event.which ===13){
+		var todoText=$(this).val();
+		$(this).val("");
+        $('ul').append("<li><span>X</span> "+todoText+"</li>");  
+
+	}
 });
+
+
+
+
+// $("ul").click(function(){
+// 	alert("ul");
+// });
+
+
+// $("#container").click(function(){
+// 	alert("container");
+// });
+
+// $("body").click(function(){
+// 	alert("body");
+// });
